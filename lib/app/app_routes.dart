c2f0e83551cc688053ee11848e14858d8380ab52
Md Rashedul Islam <santorasu09@gmail.com>
2025-06-24 22:21:05 +0600

    Added movie details page

--- lib/app/app_routes.dart
+++ lib/app/app_routes.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:movie_streaming_app/features/auth/ui/screens/splash_screen.dart';
+import 'package:movie_streaming_app/features/common/ui/screens/main_bottom_nav_screen.dart';
+import 'package:movie_streaming_app/features/movie_details/ui/movie_details_screen.dart';
 
 class AppRoutes {
   static Route<dynamic> routes(RouteSettings settings) {
     late final Widget screenWidget;
     if (settings.name == SplashScreen.name) {
       screenWidget = const SplashScreen();
+    } else if (settings.name == MainBottomNavScreen.name) {
+      screenWidget = MainBottomNavScreen();
+    }else if (settings.name == MovieDetailsScreen.name) {
+      screenWidget = MovieDetailsScreen();
     }
     return MaterialPageRoute(builder: (context) => screenWidget);
   }
